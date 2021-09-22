ff15106faa44caa721eb75b25c8289d369af9b5c
Emil Enev <emil.enev@abv.bg>
2021-09-22 20:58:24 +0300

    feat: add counter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  let [count, setCount] = useState(1);
+
+  useEffect(() => {
+    document.title = `Count (${count})`;
+  }, [count])
+
   return (
     <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
+      <div className="counter">
+        <h2>Count ({count})</h2>
+        <button onClick={() => setCount(count + 1)}>Count ({count})</button>
       </div>
     </div>
   );
